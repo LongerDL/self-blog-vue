d1a0aa0a2e5f46f62260bbd46a33fc75d4c3dff1
LongerDL <1780105381@qq.com>
2022-05-19 15:15:09 +0800

    third commit

--- src/mock/banner.js
+++ src/mock/banner.js
@@ -6,22 +6,22 @@ Mock.mock("/api/banner", "get", {
   data: [
     {
       id: "1",
-      midImg: "@/assets/middle1.jpg",
-      bigImg: "@/assets/big1.jpg",
+      midImg: "https://s2.loli.net/2022/05/19/MN6uedHfX4wL1sG.jpg",
+      bigImg: "https://s2.loli.net/2022/05/19/esnVQ5ULBj1d6Mk.jpg",
       title: "凛冬将至",
       description: "人唯有恐惧的时候方能勇敢",
     },
     {
       id: "2",
-      midImg: "@/assets/middle3.jpg",
-      bigImg: "@/assets/big2.jpg",
+      midImg: "https://s2.loli.net/2022/05/19/RPxhaAlK7dM2yCf.jpg",
+      bigImg: "https://s2.loli.net/2022/05/19/6ZRotm2InHx1qCr.jpg",
       title: "血火同源",
       description: "如果我回头，一切都完了",
     },
     {
       id: "3",
-      midImg: "@/assets/middle3.jpg",
-      bigImg: "@/assets/big3.jpg",
+      midImg: "https://s2.loli.net/2022/05/19/i9CKtjWvc2AXylI.png",
+      bigImg: "https://s2.loli.net/2022/05/19/gtMI7oN3TuUxLAC.jpg",
       title: "听我怒吼",
       description: "兰尼斯特有债必偿",
     },
